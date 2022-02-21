81e7d42ba688ab4d912d9c8ad6fd51031e65f4e8
Heng Ye <heng@yeheng.org>
2022-02-21 17:08:30 -0500

    Update the sparse

--- sparse/usr/bin/mount-dynparts.sh
+++ sparse/usr/bin/mount-dynparts.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # Verbose script for mounting dynamic partitions
-# Credit to: https://github.com/SailfishOS-vayu/droid-config-vayu/blob/master/sparse/usr/bin/droid/droid-hal-early-init.sh
+ 
 # Edit partitions according to your fixup-mountpoints, fstab and dynamic partitions layout
 super_part=/dev/sda25
+# Partitions mounted in /vendor/*
+#modem_part=/dev/sde52
+#dsp_part=/dev/sde48
+#bluetooth_part=/dev/sde27
 
 # Partition Metadata
 metadata_part=/dev/sda24
@@ -22,10 +26,17 @@ mkdir -p /system_root /system_ext /vendor /odm2 /product /mnt /metadata
 dmesg_info "$(mount -v -o ro /dev/mapper/dynpart-system  /system_root)"
 dmesg_info "$(mount --bind /system_root/system /system)"
 dmesg_info "$(mount -v -o ro /dev/mapper/dynpart-system_ext /system_ext)"
-dmesg_info "$(mount -v -o ro /dev/mapper/dynpart-vendor  /vendor)""
+dmesg_info "$(mount -v -o ro /dev/mapper/dynpart-vendor  /vendor)"
 dmesg_info "$(mount -v -o ro /dev/mapper/dynpart-odm   /odm2)"
 dmesg_info "$(mount -v -o ro /dev/mapper/dynpart-product /product)"
+
+dmesg_info "$(mount --bind /odm2/etc /odm/etc)"
  
+#dmesg_info "Mount /vendor/*"
+#dmesg_info "$(mount -v $modem_part      /vendor/firmware_mnt)"
+#dmesg_info "$(mount -v $dsp_part        /vendor/dsp)"
+#dmesg_info "$(mount -v $bluetooth_part  /vendor/bt_firmware)"
+
 dmesg_info "Mount metadata*"
 dmesg_info "$(mount -v $metadata_part   /metadata)"
  
